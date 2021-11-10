4b53b7d99d93f68b603d7c8e4b0f809d99729a5a
humbertomoraes <humbertoccmoraes@yahoo.com.br>
2021-11-10 00:37:20 -0300

    checkpoint 1 utilizando function

--- Aula 9/checkpoint_1.js
+++ Aula 9/checkpoint_1.js
@@ -14,53 +14,92 @@
 // - Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
 // - No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!".
 
-// Desta forma se atribui os valores as variáveis e executa o código, ele percorre as condições e responde apenas o que se encaixar naquela condição
 
-let prato = "Brigadeiro";
-let tempoUsuario = 24;
 
-if (prato == "Pipoca" && tempoUsuario > 30) {
-    console.log("kabum");
-} else if (prato == "Pipoca" && tempoUsuario > 20 && tempoUsuario <= 30) {
-    console.log("A comida queimou!!!");
-} else if (prato == "Pipoca" && tempoUsuario < 10) {
-    console.log("tempo insuficiente");
-} else if (prato == "Pipoca" && tempoUsuario >= 10 && tempoUsuario <= 20) {
-    console.log("Prato pronto, bom apetite!!!");
-} else if (prato == "Macarrão" && tempoUsuario > 24) {
-    console.log("kabum");
-} else if (prato == "Macarrão" && tempoUsuario > 16 && tempoUsuario <= 24) {
-    console.log("A comida queimou!!!");
-} else if (prato == "Macarrão" && tempoUsuario < 8) {
-    console.log("tempo insuficiente");
-} else if (prato == "Macarrão" && tempoUsuario >= 8 && tempoUsuario <= 16) {
-    console.log("Prato pronto, bom apetite!!!");
-} else if (prato == "Carne" && tempoUsuario > 45) {
-    console.log("kabum");
-} else if (prato == "Carne" && tempoUsuario > 30 && tempoUsuario <= 45) {
-    console.log("A comida queimou!!!");
-} else if (prato == "Carne" && tempoUsuario < 15) {
-    console.log("tempo insuficiente");
-} else if (prato == "Carne" && tempoUsuario >= 15 && tempoUsuario <= 30) {
-    console.log("Prato pronto, bom apetite!!!");
-} else if (prato == "Feijão" && tempoUsuario > 36) {
-    console.log("kabum");
-} else if (prato == "Feijão" && tempoUsuario > 24 && tempoUsuario <= 36) {
-    console.log("A comida queimou!!!");
-} else if (prato == "Feijão" && tempoUsuario < 12) {
-    console.log("tempo insuficiente");
-} else if (prato == "Feijão" && tempoUsuario >= 12 && tempoUsuario <= 24) {
-    console.log("Prato pronto, bom apetite!!!");
-} else if (prato == "Brigadeiro" && tempoUsuario > 24) {
-    console.log("kabum");
-} else if (prato == "Brigadeiro" && tempoUsuario > 16 && tempoUsuario <= 24) {
-    console.log("A comida queimou!!!");
-} else if (prato == "Brigadeiro" && tempoUsuario < 8) {
-    console.log("tempo insuficiente");
-} else if (prato == "Brigadeiro" && tempoUsuario >= 8 && tempoUsuario <= 16) {
-    console.log("Prato pronto, bom apetite!!!");
-} else if (prato != "Macarrão" && prato != "Pipoca" && prato != "Carne" && prato != "Feijão" && prato != "Brigadeiro") {
-    console.log("Prato inexistente");
-} else {
-    console.log("erro no código");
-}
+// Desta forma aproveitei todo o código da primeira versão, porém fiz utilizando function e ao final 
+// invoquei a function com diversos valores nas duas variáveis para testar as condições
+
+function microOndas(prato, tempoUsuario){
+    if ((prato == "Pipoca") && (tempoUsuario > 30)) {
+        return console.log("kabum");
+    } else if (prato == "Pipoca" && tempoUsuario > 20 && tempoUsuario <= 30) {
+        return console.log("A comida queimou!!!");
+    } else if (prato == "Pipoca" && tempoUsuario < 10) {
+        return console.log("tempo insuficiente");
+    } else if (prato == "Pipoca" && tempoUsuario >= 10 && tempoUsuario <= 20) {
+        return console.log("Prato pronto, bom apetite!!!");
+    } else if (prato == "Macarrão" && tempoUsuario > 24) {
+        return console.log("kabum");
+    } else if (prato == "Macarrão" && tempoUsuario > 16 && tempoUsuario <= 24) {
+        return console.log("A comida queimou!!!");
+    } else if (prato == "Macarrão" && tempoUsuario < 8) {
+        return console.log("tempo insuficiente");
+    } else if (prato == "Macarrão" && tempoUsuario >= 8 && tempoUsuario <= 16) {
+        return console.log("Prato pronto, bom apetite!!!");
+    } else if (prato == "Carne" && tempoUsuario > 45) {
+        return console.log("kabum");
+    } else if (prato == "Carne" && tempoUsuario > 30 && tempoUsuario <= 45) {
+        return console.log("A comida queimou!!!");
+    } else if (prato == "Carne" && tempoUsuario < 15) {
+        return console.log("tempo insuficiente");
+    } else if (prato == "Carne" && tempoUsuario >= 15 && tempoUsuario <= 30) {
+        return console.log("Prato pronto, bom apetite!!!");
+    } else if (prato == "Feijão" && tempoUsuario > 36) {
+        return console.log("kabum");
+    } else if (prato == "Feijão" && tempoUsuario > 24 && tempoUsuario <= 36) {
+        return console.log("A comida queimou!!!");
+    } else if (prato == "Feijão" && tempoUsuario < 12) {
+        return console.log("tempo insuficiente");
+    } else if (prato == "Feijão" && tempoUsuario >= 12 && tempoUsuario <= 24) {
+        return console.log("Prato pronto, bom apetite!!!");
+    } else if (prato == "Brigadeiro" && tempoUsuario > 24) {
+        return console.log("kabum");
+    } else if (prato == "Brigadeiro" && tempoUsuario > 16 && tempoUsuario <= 24) {
+        return console.log("A comida queimou!!!");
+    } else if (prato == "Brigadeiro" && tempoUsuario < 8) {
+        return console.log("tempo insuficiente");
+    } else if (prato == "Brigadeiro" && tempoUsuario >= 8 && tempoUsuario <= 16) {
+        return console.log("Prato pronto, bom apetite!!!");
+    } else if (prato != "Macarrão" && prato != "Pipoca" && prato != "Carne" && prato != "Feijão" && prato != "Brigadeiro") {
+        return console.log("Prato inexistente");
+    } else {
+        return console.log("erro no código")
+    }
+    }
+    
+    microOndas("Pipocas", 9)
+    microOndas("Pipoca", 9)
+    microOndas("Pipoca", 10)
+    microOndas("Pipoca", 20)
+    microOndas("Pipoca", 21)
+    microOndas("Pipoca", 30)
+    microOndas("Pipoca", 33)
+    
+    microOndas("Macarrão", 7)
+    microOndas("Macarrão", 8)
+    microOndas("Macarrão", 16)
+    microOndas("Macarrão", 17)
+    microOndas("Macarrão", 24)
+    microOndas("Macarrão", 25)
+    
+    microOndas("Carne", 14)
+    microOndas("Carne", 15)
+    microOndas("Carne", 30)
+    microOndas("Carne", 31)
+    microOndas("Carne", 45)
+    microOndas("Carne", 46)
+    
+    microOndas("Feijão", 11)
+    microOndas("Feijão", 12)
+    microOndas("Feijão", 24)
+    microOndas("Feijão", 25)
+    microOndas("Feijão", 36)
+    microOndas("Feijão", 37)
+    
+    microOndas("Brigadeiro", 7)
+    microOndas("Brigadeiro", 8)
+    microOndas("Brigadeiro", 16)
+    microOndas("Brigadeiro", 17)
+    microOndas("Brigadeiro", 24)
+    microOndas("Brigadeiro", 25)
+    
