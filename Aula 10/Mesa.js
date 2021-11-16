b0a1c5d4f735af6102df33b4100bc976e364d565
humbertomoraes <humbertoccmoraes@yahoo.com.br>
2021-11-15 23:42:56 -0300

    exercícios

--- Aula 10/Mesa.js
+++ Aula 10/Mesa.js
@@ -1,7 +1,38 @@
-let array2 = ([1,2,3])
+let teste = (["Humberto", "João", "José"])
 
-function somarArray(){
-    return console.log(array2)
-}
+// A) Acessar elementos específicos de um array.
+console.log(teste[2])
 
-somarArray(array2);
+
+// B) Modificar cada um dos elementos de um array e imprimi-los no console.
+let teste2 = teste.join()   // primeiro modificar o array por string usando o JOIN
+console.log(teste2.replace("Humberto", "Augusto"))  // replace para alterar os elementos
+console.log(teste2.replace("João", "Rodrigo"))
+console.log(teste2.replace("José", "Cesar"))
+console.log(teste2)
+
+// C) Adicionar elementos a um array.
+console.log(teste.push("Maria", "Ana"))         // PUSH adiciona elemento ao final do array
+console.log(teste.unshift("Joaquim", "Joana"))  // unshift adiciona elemento no início do array
+console.log(teste)
+
+// D) Extrair elementos de um array.
+console.log(teste.pop())    // POP elimina o último elemento
+console.log(teste.shift())  // elimina o primeiro elemento
+console.log(teste)
+
+// E) Comparar elementos de um array com os elementos de outro. 
+// ???????????????
+
+
+// Neste exercício, você terá que pensar sobre o que esses códigos retornam sem testá-lo no console.
+
+let numbers =[22, 33, 54, 66, 72]
+console.log(numbers[numbers.length])  // retorna a quantidade de elementos, porém há um erro na síntaxe com o numbers antes do []
+
+let grupoDeAmigos = [ ["Harry", "Ron", "Hermione"], ["Spiderman", "Hulk", "Ironman"], ["Penélope Glamour", "Pierre Nodoyuna","Patán"] ]
+console.log(grupoDeAmigos[1][0])    // retorna o primeiro elemento do segundo array
+
+let str = "uma string qualquer"
+let grupoDeAmigos = [ [45, 89, 0], ["Digital", "House", true], ["string", "123","false", 54, true, str] ]
+console.log(grupoDeAmigos[2][grupoDeAmigos[2].length - 1])    // Não entendi nada ????????
---
+++ Aula 11/cicloFor.js
@@ -0,0 +1,26 @@
+let volta = 10
+for (volta; volta >= 1; volta--){
+    console.log("Volta número " + volta)
+}
+
+
+// 1) Repetir como um papagaio
+// Crie um loop utilizando for que se repita 5 vezes. Dentro de cada repetição se deve mostrar no console a mensagem “Olá mundo”.
+let texto = "Olá mundo!!"
+for (num = 1; num <= 5; num++){
+    console.log(texto)
+}
+
+
+// 2) Contando números ímpares
+// Crie um loop for de 1 a 10, em que exiba no console apenas o valor das repetições de números ímpares. Dica: A ideia é que nas repetições ímpares de 0 ao 10 (1,3,5,7,9) sejam mostrados os números das repetições no console.
+
+
+
+// 3) Criando a tabuada
+// Crie uma tabuada utilizando o for. Dica: Para essa tarefa você irá precisar utilizar dois loops (for).
+let numero = 7
+for (tabuada = 1; tabuada <= 10; tabuada++){
+    console.log(numero + " x " + tabuada + " = " + tabuada * numero)
+}
+
